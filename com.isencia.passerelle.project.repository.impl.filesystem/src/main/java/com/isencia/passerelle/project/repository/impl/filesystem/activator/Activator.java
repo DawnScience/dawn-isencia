c961a853ab642218bad09ba5c9cb964b6f5b9720
Olof Svensson <svensson@esrf.fr>
2013-03-22 13:53:45 +0100

    Temporary fix for SCI-984: submodels folder is in funny place

--- com.isencia.passerelle.project.repository.impl.filesystem/src/main/java/com/isencia/passerelle/project/repository/impl/filesystem/activator/Activator.java
+++ com.isencia.passerelle.project.repository.impl.filesystem/src/main/java/com/isencia/passerelle/project/repository/impl/filesystem/activator/Activator.java
@@ -1,5 +1,7 @@
 package com.isencia.passerelle.project.repository.impl.filesystem.activator;
 
+import java.io.File;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
@@ -19,8 +21,11 @@ public class Activator implements BundleActivator {
   private static Activator plugin;
 
   public void start(BundleContext context) throws Exception {
-    String rootFolderPath = System.getProperty("com.isencia.passerelle.project.root", "C:/temp/passerelle-repository");
-    String submodelPath = System.getProperty("com.isencia.passerelle.submodel.root", "C:/temp/submodel-repository");
+    File userHome              = new File(System.getProperty("user.home"));
+    File defaultRootFolderPath = new File(userHome, ".passerelle/passerelle-repository");
+    File defaultSubmodelPath   = new File(userHome, ".passerelle/submodel-repository");
+    String rootFolderPath      = System.getProperty("com.isencia.passerelle.project.root", defaultRootFolderPath.getAbsolutePath());
+    String submodelPath        = System.getProperty("com.isencia.passerelle.submodel.root", defaultSubmodelPath.getAbsolutePath());
     repoSvc = new FileSystemBasedRepositoryService(rootFolderPath, submodelPath);
     repoSvcReg = context.registerService(RepositoryService.class.getName(), repoSvc, null);
 
